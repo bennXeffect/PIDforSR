935bb9ae4f61d337539c99e795d9a1353e26dcae
bennXeffect <67720798+bennXeffect@users.noreply.github.com>
2023-05-16 01:35:16 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,28 @@
-/********************************************************
- * PID RelayOutput Example
- * Same as basic example, except that this time, the output
- * is going to a digital pin which (we presume) is controlling
- * a relay.  the pid is designed to Output an analog value,
- * but the relay can only be On/Off.
- *
- *   to connect them together we use "time proportioning
- * control"  it's essentially a really slow version of PWM.
- * first we decide on a window size (5000mS say.) we then
- * set the pid to adjust its output between 0 and that window
- * size.  lastly, we add some logic that translates the PID
- * output into "Relay On Time" with the remainder of the
- * window being "Relay Off Time"
- ********************************************************/
-
 
 #include <Arduino.h>
 #include <PID_v1.h>
-#include <SPI.h>
+//#include <SPI.h>
 #include <OneWire.h>
-#include <Adafruit_NeoPixel.h>
+//#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 #include <DallasTemperature.h>
 #include <U8g2lib.h>
-//#include <U8x8lib.h>
 #include <Wire.h>
-//#include "U8glib.h"
+#include "OneButton.h"
 
 
 
-#define ONE_WIRE_BUS 2
-#define NUM_LEDS 2
-#define DATA_PIN 1 //LED pin data
+#define ONE_WIRE_BUS D5
+#define NUM_LEDS 3
+#define DATA_PIN D2 //LED pin data
 
-#define PIN_INPUT 0
-#define RELAY_PIN 6
+#define PIN_INPUT A0
+#define RELAY_PIN D4
 
-#define BUTTON_PIN 4
-#define reset_P 5
+#define BUTTON_PIN D6
+//#define reset_P 5
 
-
-// Define the array of leds
-//CRGB leds[NUM_LEDS];
+OneButton button1(BUTTON_PIN, true);
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -50,80 +32,73 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
-
 int pot;
-
+bool WORKING_STATE = 0; //system ON or OFF ?
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
+
+float tempC = 20; //the temp from the sensor
 
 //Specify the links and initial tuning parameters
 double Kp=2, Ki=5, Kd=1;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
-int WindowSize = 5000;
+unsigned int WindowSize = 3500;
 unsigned long windowStartTime;
 
+unsigned char beat = 0;
+int blinkPace = 15; //blinking pace, the bigger the slower.
 
-//U8G2_SSD1306_128X32_UNIVISION_1_SW_I2C(U8G2_R0, /* clock=*/ 9, /* data=*/ 7, /* reset=*/ U8X8_PIN_NONE) [page buffer, size = 128 bytes];
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 9, /* data=*/ 8);   // pin remapping with ESP8266 HW I2C
 
-//U8G2_SSD1306_128X32_UNIVISION_2_SW_I2C(U8G2_R0, clock, data [, reset]) [page buffer, size = 256 bytes]
-//U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C(rotation, clock, data [, reset]) [full framebuffer, size = 512 bytes]
-//U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C(rotation, [reset [, clock, data]]) [page buffer, size = 128 bytes]
-//U8G2_SSD1306_128X32_UNIVISION_2_HW_I2C(rotation, [reset [, clock, data]]) [page buffer, size = 256 bytes]
-//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C(rotation, [reset [, clock, data]]) [full framebuffer, size = 512 bytes]
-//U8G2_SSD1306_128X32_UNIVISION_1_2ND_HW_I2C(rotation, [reset]) [page buffer, size = 128 bytes]
-//U8G2_SSD1306_128X32_UNIVISION_2_2ND_HW_I2C(U8G2_R0, [reset]) [page buffer, size = 256 bytes]
-//U8G2_SSD1306_128X32_UNIVISION_F_2ND_HW_I2C(rotation, [reset]) [full framebuffer, size = 512 bytes]
-
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 9, /* data=*/ 8);   // pin remapping with ESP8266 HW I2C
-//U8G2_SSD1306_128X32_WINSTAR_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
-
-//U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI
-
-//U8X8_SSD1306_128X32_UNIVISION_2ND_HW_I2C ([reset_P]);
+static unsigned char u8g_heat[] = {
+  0x30, 0xC6, 0x00, 0x30, 0xC6, 0x00, 0x10, 0x42, 0x00, 0x18, 0x63, 0x00,
+  0x18, 0x63, 0x00, 0x18, 0x63, 0x00, 0x38, 0xE7, 0x00, 0x30, 0xC6, 0x00,
+  0x30, 0xC6, 0x00, 0x10, 0x42, 0x00, 0x00, 0x00, 0x00, 0xFC, 0xFF, 0x03,
+  0xFF, 0xFF, 0x0F, 0xDB, 0xB6, 0x0D, 0xDB, 0xB6, 0x0D, 0xDB, 0xB6, 0x0D,
+  0xDB, 0xB6, 0x0D, 0xDB, 0xB6, 0x0D, 0xFF, 0xFF, 0x0F, 0xFC, 0xFF, 0x03,
+};
 
 
-///////////////////neopixel
+CRGB leds[NUM_LEDS];
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(2, DATA_PIN, NEO_GRB + NEO_KHZ800);
+void clic1(){//to do when simple clic
+  WORKING_STATE = !WORKING_STATE;
+   Serial.print("Working state is ");
+  Serial.println(WORKING_STATE);
+  delay(150);
 
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
+}
+void longPress1(){
+}
+void longPressStart1(){
+}
+void doubleClic1(){
+}
 
+String tempInt;
+String SetTemp;
 
+void setup(){
 
+  Serial.begin(115200);
 
-/////////////////////end neopixel
+   LEDS.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS)
+  //LEDS.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS)
+      .setTemperature(0xB5D6FF)
+  .setCorrection(TypicalSMD5050);
+  LEDS.setBrightness(255);
 
+  u8g2.begin();
+  u8g2.clearBuffer(); // clear the internal memory
 
-
-
-
-
-void setup()
-{
-
-strip.begin();
-  strip.setBrightness(50);
-  strip.show(); // Initialize all pixels to 'off'
-
-
-/////////////////PID///////////////////
+  /////////////////PID///////////////////
   windowStartTime = millis();
 
   //initialize the variables we're linked to
-  Setpoint = 100;
+  Setpoint = 20;
 
   //tell the PID to range between 0 and the full window size
   myPID.SetOutputLimits(0, WindowSize);
@@ -138,54 +113,97 @@ strip.begin();
 
 pinMode(RELAY_PIN, OUTPUT);
 
+
+  button1.attachClick(clic1);
+  //button1.attachDoubleClick(doubleClic1);
+  //button1.attachLongPressStart(longPressStart1);
+//  button1.attachLongPressStop(longPressStop1);
+  //button1.attachDuringLongPress(longPress1);
+  //button1.setClickTicks(300);
+
+
+//sensors.setResolution(9);
 }
 
-void loop()
-{
-/*
-//////////////// DALLAS SHIT NO DALLAS LIB /////////////////
-byte i;
-byte data[12];
-
-int16_t raw;
-
-float t;
-
-TemperatureSensor.reset(); // reset one wire buss
-TemperatureSensor.skip(); // select only device
-
-TemperatureSensor.write(0x44); // start conversion
-
-delay(500); // wait for the conversion
-
-TemperatureSensor.reset();
-TemperatureSensor.skip();
-
-TemperatureSensor.write(0xBE); // Read Scratchpad
-
-for ( i = 0; i < 9; i++) { // 9 bytes
-
-data[i] = TemperatureSensor.read();
-
+void loop(){
+ button1.tick(); // check the button state
+ SetTemp = "";
+ SetTemp += Setpoint;
+    beat = sin8(millis() / blinkPace); //put the blinking pace
+  u8g2.clearBuffer(); // clear the internal memory
+  u8g2.setFont(u8g2_font_5x7_tf);
+  if (WORKING_STATE == 1){
+    u8g2.drawStr(102, 7, "ON!");
+}
+else{
+    u8g2.drawStr(102, 7, "OFF");
 }
 
-raw = (data[1] << 8) | data[0];
-t = (float)raw / 16.0;
+ u8g2.drawLine(92, u8g2.getDisplayHeight() / 2 - 10, 92, u8g2.getDisplayHeight() / 2 + 10);
+ u8g2.setFont(u8g2_font_5x7_tf);
+ //u8g2.drawStr(1, 7, "Target");
+ u8g2.drawStr(52, 7, "Actual");
+ 
 
-//////////////// END DALLAS SHIT NO DALLAS LIB /////////////////
-*/
+ //  u8g2.setDrawColor(0);       // errase mode
+ // u8g2.drawBox(100, 16, 128, 22); // clear the txt
+ // u8g2.setDrawColor(1); // write mode
+ u8g2.setFont(u8g2_font_fur30_tn);
+ u8g2.setCursor(0, 31);
+ //  u8g2.drawStr(70,32,tempInt.c_str());
+ u8g2.print(Setpoint, 0);
+ u8g2.setFont(u8g2_font_fur20_tn);
+ u8g2.setCursor(51, 31);
+ u8g2.print(tempC,0);
+  
+//    u8g2.setCursor(55, 32);
+ // u8g2.print(Setpoint.c_str());
+    //u8g2.drawStr(0, 32, SetTemp.c_str());
+    
 
+    /*
+    //////////////// DALLAS SHIT NO DALLAS LIB /////////////////
+    byte i;
+    byte data[12];
+    int16_t raw;
+    float t;
+    TemperatureSensor.reset(); // reset one wire buss
+    TemperatureSensor.skip(); // select only device
+    TemperatureSensor.write(0x44); // start conversion
+    delay(500); // wait for the conversion
+    TemperatureSensor.reset();
+    TemperatureSensor.skip();
+    TemperatureSensor.write(0xBE); // Read Scratchpad
+    for ( i = 0; i < 9; i++) { // 9 bytes
+    data[i] = TemperatureSensor.read();
+    }
+    raw = (data[1] << 8) | data[0];
+    t = (float)raw / 16.0;
+    //////////////// END DALLAS SHIT NO DALLAS LIB /////////////////
+    */
 
-//////////////// DALLAS SHIT /////////////////
-sensors.requestTemperatures(); // Send the command to get temperatures
+    //////////////// DALLAS SHIT /////////////////
+    EVERY_N_SECONDS(1)
+    {
+      sensors.setResolution(12);
+
+sensors.setWaitForConversion(false);  // makes it async
+  sensors.requestTemperatures();
+  sensors.setWaitForConversion(true);
+
+//      sensors.requestTemperatures(); // Send the command to get temperatures
+}
 // We use the function ByIndex, and as an example get the temperature from the first sensor only.
-  float tempC = sensors.getTempCByIndex(0);
+  tempC = sensors.getTempCByIndex(0);
+  tempC = 30;
 
+  tempInt = "";
+  tempInt += floor(tempC);
   // Check if reading was successful
   if (tempC != DEVICE_DISCONNECTED_C)
   {
-    Serial.print("Temperature for the device 1 (index 0) is: ");
-    Serial.println(tempC);
+  //  Serial.print("Temperature for the device 1 (index 0) is: ");
+  //  Serial.println(tempC);
   }
   else
   {
@@ -194,22 +212,45 @@ sensors.requestTemperatures(); // Send the command to get temperatures
 
 //////////////// END DALLAS SHIT /////////////////
 
+Setpoint=map(analogRead(PIN_INPUT),0,1023,25,80);
+Input = tempC;
+myPID.Compute();
 
+if(WORKING_STATE==1){ //system up and running, ON state
+    blinkPace = 5;
+ for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i]  = CHSV(215, 255, map(beat,0,255,40,255));
+      }
 
-
-  Input = analogRead(PIN_INPUT);
-  myPID.Compute();
-
-  /************************************************
-   * turn the output pin on/off based on pid output
-   ************************************************/
-  if (millis() - windowStartTime > WindowSize)
-  { //time to shift the Relay Window
-    windowStartTime += WindowSize;
+    /************************************************
+     * turn the output pin on/off based on pid output
+     ************************************************/
+    if (millis() - windowStartTime > WindowSize)
+    { // time to shift the Relay Window
+      windowStartTime += WindowSize;
   }
-  if (Output < millis() - windowStartTime) digitalWrite(RELAY_PIN, HIGH);
-  else digitalWrite(RELAY_PIN, LOW);
-
+  if (Output < millis() - windowStartTime) {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+  else{
+    digitalWrite(RELAY_PIN, HIGH);
+    leds[NUM_LEDS/2]  = CHSV(0, 255, 255);
+    u8g2.setFont(u8g2_font_unifont_t_symbols);
+   // u8g2.drawGlyph(102, 30, 0x2615);
+    u8g2.drawXBM(99, 12, 20, 20, u8g_heat);
+    u8g2.setFont(u8g2_font_5x7_tf);
+  }
 }
-
-
+else{
+  blinkPace = 15; //slow the blink pace, cuz the system is sleeping
+ for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i]  = CHSV(185, 200, map(beat,0,255,15,255));
+      }
+ 
+  //to do when off
+}
+Serial.println(beat);
+  //Serial.println(digitalRead(RELAY_PIN));
+  FastLED.show();
+  u8g2.sendBuffer();
+}
